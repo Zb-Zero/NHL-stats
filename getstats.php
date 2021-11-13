b2dab6f521c2d212522a1d9d0f1034e418d30fe4
Zb-Zero <74880585+Zb-Zero@users.noreply.github.com>
2021-11-13 07:00:43 -0500

    Update getstats.php

--- getstats.php
+++ getstats.php
@@ -4,7 +4,7 @@
 <?php
 $date = $_POST['year'];
 
-echo htmlspecialchars($_POST['https://statsapi.web.nhl.com/api/v1/standings?season=2018']);
+echo htmlspecialchars($_POST['https://statsapi.web.nhl.com/api/v1/standings?season=20182019']);
 
  ?>
 
