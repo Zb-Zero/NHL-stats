fc93c1396de75453717bb281a9d9d6edc10bff30
Zb-Zero <74880585+Zb-Zero@users.noreply.github.com>
2021-11-13 06:19:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
 <body>
 <h1>NHL Stats Page</h1>
 
-<form method="post" action='https://statsapi.web.nhl.com/api/v1/standings?season=2018'>
+<form method="get" action='https://statsapi.web.nhl.com/api/v1/standings?season=2018'>
     <input type="text" name="text" />
     <input type="submit" value="submit" />
 </form>
 <?php
-if (isset($_POST['text'])) {
-    echo $_POST['text'];
+if (isset($_GET['text'])) {
+    echo $_GET['text'];
 }
 ?>
 
