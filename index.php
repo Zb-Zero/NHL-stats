84a7e4e320e21235bd9c6f5372dbe3e99d595103
Zb-Zero <74880585+Zb-Zero@users.noreply.github.com>
2021-11-13 07:34:16 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<html>
+<head>
+
+</head>
+<body>
+<h1>NHL Stats Page</h1>
+<p>Enter the year here</p>
+
+<?php
+session_start();
+$_SESSION['year'] = $date
+?>
+
+<form method="post" action='getstats.php'>
+    <input type="text" name="year" />
+    <input type="submit" value="submit" />
+</form>
+
+
+
+
+
+
+
+</body>
+
+</html>
